247f2ca75b20e31fb3d5a709104efb2f564150a7
Austin Bridgewater <abridgewater34@gmail.com>
2021-10-30 15:22:50 -0400

    Add project files.

---
+++ 72HrChallenge/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _72HrChallenge.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ 72HrChallenge/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_72HrChallenge.Startup))]
+
+namespace _72HrChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
