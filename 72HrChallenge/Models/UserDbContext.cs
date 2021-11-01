7f26f6e287db9233d2f09eb87065dd309028a72e
Austin Bridgewater <abridgewater34@gmail.com>
2021-11-01 09:27:56 -0400

    Updated Connection string

--- 72HrChallenge/Models/UserDbContext.cs
+++ 72HrChallenge/Models/UserDbContext.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace _72HrChallenge.Models
 {
-    public class UserDbContext
+    public class UserDbContext : DbContext
     {
+        public UserDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Users> Users {get; set; }
     }
 }
