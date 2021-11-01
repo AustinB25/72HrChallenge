501c7a8ede42eb5c1b9736cfb81a2c86b3d4700c
Austin Bridgewater <abridgewater34@gmail.com>
2021-11-01 10:03:29 -0400

    Had to rebuild. VS community updated in between commits

---
+++ 72HrChallenge/Models/UserDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _72HrChallenge.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
