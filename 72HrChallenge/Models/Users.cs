a310e7e46cb3faf0c67622c39b378b75a51a98e7
Austin Bridgewater <abridgewater34@gmail.com>
2021-11-01 09:16:45 -0400

    Added DbContext for Users

--- 72HrChallenge/Models/Users.cs
+++ 72HrChallenge/Models/Users.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace _72HrChallenge.Models
 {
-    public class UserModel
+    public class Users
     {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string FullName { get { return FirstName + " " + LastName; } }
+        [Required]
+        public string UserName { get; set; }
+        //public virtual List<Posts> {get, set,} = new List<Posts>        
     }
 }
