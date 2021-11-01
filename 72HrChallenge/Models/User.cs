3ff7f959617b3caaca7b9104c8416c2f4c29a25d
Austin Bridgewater <abridgewater34@gmail.com>
2021-11-01 10:24:15 -0400

    Added UserGetAll Method

--- 72HrChallenge/Models/User.cs
+++ 72HrChallenge/Models/User.cs
@@ -8,7 +8,7 @@ namespace _72HrChallenge.Models
 {
     public class User
     {
-        [Key, Required]
+        [Key]
         public int UserId { get; set; }
         [Required]
         public string FirstName { get; set; }
