5d15fd327b7ebd0371c9e93d9e990a9120e3f48c
Lauren March <88299368+laurenbmarch@users.noreply.github.com>
2021-11-01 22:26:03 -0400

    postmodelcreated

--- 72HrChallenge/Models/Post.cs
+++ 72HrChallenge/Models/Post.cs
@@ -7,5 +7,17 @@ namespace _72HrChallenge.Models
 {
     public class Post
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public virtual List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Guid AuthorId { get; set; }
     }
 }
