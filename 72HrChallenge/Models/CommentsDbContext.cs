6e310004f92e8570fc8da1cda360448df2e70c3e
Jacob <jacobeljones56@gmail.com>
2021-11-01 22:12:00 -0400

    Comment section all complete, pushing for review

---
+++ 72HrChallenge/Models/CommentsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _72HrChallenge.Models
+{
+    public class CommentsDbContext : DbContext
+    {
+        public CommentsDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Comments> Comments { get; set; }
+
+    }
+}
